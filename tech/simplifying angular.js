dfae3529b571db858d576acb0a9ff986b0865a4f
Nishant Singh <nishant.singh87@gmail.com>
2015-07-31 19:27:59 +0530

    separating out section on single page app basics.

--- tech/simplifying angular.js
+++ tech/simplifying angular.js
@@ -1,5 +1,38 @@
 to do : 
 
+Single page apps : 
+a url in a sinlge page app looks something like this 
+- "http://angular-ui.github.io/ui-router/sample/#/contacts"
+
+A Deeper Look
+ - location href 		: [origin, pathname, hash]
+ - location origin   	: "http://angular-ui.github.io" // server, hosting our app
+ - location pathname 	: "/ui-router/sample/"          // at this path server returns our app
+ - location hash     	: "#/contacts"                  // app uses it to navigate between pages
+ 
+ Single Page Apps, 
+ - app launches when on entering (origin + pathname) in address bar.
+ - when (origin + pathname) changes, app is closed.
+ - location hash decides which page to render
+ - on launch, app looks [cookie/location-hash/local-storage], and decides what to render next
+ - e.g. if session-id is missing in cookie, redirects to login page.
+        or open a particular page for the user.
+        or shows progress bar unless it downloads and add some data to local storage.
+
+ e.g         
+ - "#/<route>" 	: locaiton hash represents which page to show
+ - "#/"  		: usually represents home state [home/default view of app]
+ 
+  Importance of location hash in a single page app
+   -> location.hash is the part after "#" in current url (e.g. http://angular-ui.github.io/ui-router/sample/#/contacts)
+     - it allows navigation to pages, within a sigle page app.
+     - when part before # changes, page is relaoded(browsing context is lost)
+     - browsing context encapsulates dom, js libraries, js objects in memory.
+     - when page reloaded, browsing context is lost
+     - with browsing context, we loose : dom, js libs, js objects in memory
+--------------------------------------------------------------------------
+
+
 1. create an agular app
  - every app is just another module.
  - every module has 
@@ -16,47 +49,18 @@ to do :
  	// return new module
  }
   
-2. create a home page(#/)
+2. Create a home page(#/)
   - sub-route is the part after the "#", 
   - each sub-route represents a page in app
   - we add new page by adding a config for the page, and declaring an angular controller to use with it.
   - we add 
 
-window.location: a deeper look
- - page-url          : http://angular-ui.github.io/ui-router/sample/#/contacts
- - location.origin   : "http://angular-ui.github.io" // server
- - location.pathname : "/ui-router/sample/"          // path at which ur web server, returns html page]
- - location.hash     : "#/contacts"                  // information of current page, appended to url
- 
- Single Page Apps, 
- - identified by combination: [origin + pathname]
- - app launches when (origin + pathname) is invoked from adress bar 
- - when (origin + pathname) changes, app is closed.
- - app looks at part after [#, cookie, storage], and decides what to render next
- - e.g. if session-id is missing in cookie, redirects to login page.
-        or open a particular page for the user.
-        
- - "#/<route>" hash is used to store the routes, for navigating between pages ().
- - "#/"  : usually represent home view (u see when app starts for first time)
- 
-  Importance of location hash in a single page app
-   -> location.hash is the part after "#" in current url (e.g. http://angular-ui.github.io/ui-router/sample/#/contacts)
-     - it allows navigation to pages, within a sigle page app.
-     - when part before # changes, page is relaoded(browsing context is lost)
-     - browsing context encapsulates dom, js libraries, js objects in memory.
-     - when page reloaded, browsing context is lost
-     - with browsing context, we loose : dom, js libs, js objects in memory
      
 3.display a list of items on home page
 4. create another view to edit it.
 5. another one to view it
 
-
---> googling tip : remove search result older than a year, always !!!
-
-
-2. create home route ("/")
-   register routes in app config
+-  How to register
    angular app can be configured like : 
 
    	//assumption
